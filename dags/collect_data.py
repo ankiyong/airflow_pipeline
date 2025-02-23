a27bd9f89f093fd8fa28e505f847c353bd0dc558
aky <aky@ubuntu.myguest.virtualbox.org>
2025-02-23 22:44:52 +0900

    add: dag 파일 추가

---
+++ dags/collect_data.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.models.variable import Variable
+import requests,json
+from datetime import datetime, timedelta
+
+last_value = 10
+
+def get_order_data_after_last_value(last_value):
+    url = f"http://220.70.6.76:8000/orders/id/{last_value}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print("Connect Success")
+        return response.json()
+    
+
+with DAG(
+    'data_pipeline',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='A simple data_pipeline DAG',
+    schedule_interval='1 * * * *',
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=['example'],
+) as dag:
+
+    t1 = PythonOperator(
+        task_id='get-order-data',
+        python_callable=get_order_data_after_last_value,
+        op_args=[last_value]
+    )
+
+t1
