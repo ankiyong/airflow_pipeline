ddbcade846b7f30414753a2206b64738d2dc5037
An Ki Yong <85871256+ankiyong@users.noreply.github.com>
2025-03-01 00:18:53 +0900

    Update sub_data.py

--- dags/sub_data.py
+++ dags/sub_data.py
@@ -81,9 +81,7 @@ save_to_json=PythonOperator(
 
 spark_process = SparkKubernetesOperator(
     task_id="spark-process",
-    trigger_rule="all_success",
     depends_on_past=True,
-    retries=3,
     application_file="olist_spark.yaml",
     namespace="spark-jobs",
     kubernetes_conn_id="kubernetes-conn-default",
