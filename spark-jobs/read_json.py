2e2c23e96602c4af5eba2e3b1fb7d3b2ca105596
aky <aky@ubuntu.myguest.virtualbox.org>
2025-03-03 12:43:20 +0900

    fix: json read 방식 변경

--- spark-jobs/read_json.py
+++ spark-jobs/read_json.py
@@ -6,15 +6,16 @@ if __name__ == "__main__":
     if os.path.exists(file_path_sec):
         print("################# 존재합니다. spark 시작합니다 #################")
         spark = SparkSession.builder.appName("DataProcessing").getOrCreate()
-        with open(file_path_sec,"r",encoding="utf-8") as f:
-            raw_data = json.load(f)
-        parsed_data = [json.loads(item) for item in raw_data]
-
-
-        df = spark.parallelize(parsed_data).map(lambda x: json.dumps(x))
-        df = spark.read.json(df)
+        # with open(file_path_sec,"r",encoding="utf-8") as f:
+        #     raw_data = json.load(f)
+        # parsed_data = [json.loads(item) for item in raw_data]
+        data = []
+        with open(file_path_sec, 'r') as f:
+            data.extend(json.load(f))
+            df = spark.createDataFrame(data[0])
+            df.show()
         # multiline_df = spark.read.option("multiline","true") .json(parsed_data)
-        multiline_df.printSchema()
-        multiline_df.show()
+        # multiline_df.printSchema()
+        # multiline_df.show()
     else:
         print("################# 존재하지 않습니다 #################")
