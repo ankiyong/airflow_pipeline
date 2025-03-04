1038064a97c0ad4ba0951641065493d641d3b48c
An Ki Yong <85871256+ankiyong@users.noreply.github.com>
2025-03-04 20:27:13 +0900

    Update read_json.py

--- spark-jobs/read_json.py
+++ spark-jobs/read_json.py
@@ -5,8 +5,20 @@ if __name__ == "__main__":
     file_path_sec = "/opt/spark/data/xcom_data.json"
     if os.path.exists(file_path_sec):
         print("################# 존재합니다. spark 시작합니다 #################")
-        spark = SparkSession.builder.appName("DataProcessing") \
-                .getOrCreate()
+        gcs_bucket = "olist_buckets"
+        parquet_path = f"gs://{gcs_bucket}/orders/orders.parquet"
+        spark = (
+            SparkSession.builder
+            .appName("pyspark-gcs-connection")
+            .master("local[*]")
+            .config("spark.jars", "/opt/spark/data/gcs-connector-hadoop3-2.2.9-shaded.jar")
+            .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+            .config("spark.hadoop.google.cloud.auth.service.account.enable", "true")
+            .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", "/opt/spark/data/key.json")
+            .getOrCreate()
+            )
+
+        df = spark.read.parquet("[GCS gsutil URI 주소]")
 
         with open(file_path_sec, 'r') as f:
             json_data = json.load(f)
@@ -16,6 +28,7 @@ if __name__ == "__main__":
                 data.append(json.loads(i_replace))
             df = spark.createDataFrame(data)
             df.show()
-            json_list = [row.asDict() for row in df.collect()]
+
+            df.write.format("parquet").save(parquet_path)
     else:
         print("################# 존재하지 않습니다 #################")
