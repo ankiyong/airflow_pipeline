b428e672df4b761ab3cce7999e8750b69f926b43
aky <aky@ubuntu.myguest.virtualbox.org>
2025-03-11 15:26:34 +0900

    fix: data process DAG 수정

--- dags/sub_data.py
+++ dags/sub_data.py
@@ -9,9 +9,6 @@ from airflow.operators.dummy import DummyOperator
 from datetime import datetime, timedelta
 import csv
 
-PROJECT_ID = "data-streaming-olist"
-SUBSCRIPTION_NAME = "order_data-sub"
-
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
@@ -35,39 +32,23 @@ def decide_next_task(**context):
         return "save_to_json"
     return "end_task"
 
-def message_cnt():
+def get_data():
     f = open("/opt/airflow/logs/publish_last_value.txt",'r')
     publish_last_value = f.read()
-    return publish_last_value
-
-def save_to_csv(**context):
-    data = context['task_instance'].xcom_pull(key="query_results")
-    csv_file_path = "/opt/airflow/logs/xcom_data.csv"
-    with open(csv_file_path, "w", newline="") as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerows(data)
-
-publish_last_value = message_cnt()
-
-get_data = PostgresOperator(
-    task_id = "postgres_check",
-    postgres_conn_id = "olist_postgres_conn",
-    parameters={"publish_last_value": publish_last_value},
-    sql = f"""
-        SELECT
-            *
-        FROM
-            pubsub.olist_pubsub
-        WHERE
-            publish_time > TO_TIMESTAMP('{publish_last_value}', 'YYYY-MM-DD HH24:MI:SS.MS')
-        """
-)
-
-save_to_csv = PythonOperator(
-    task_id = "save_to_json",
-    python_callable = save_to_csv,
-    provide_context = True
-)
+    postgres_task = PostgresOperator(
+        task_id = "postgres_check",
+        postgres_conn_id = "olist_postgres_conn",
+        parameters={"publish_last_value": publish_last_value},
+        sql = f"""
+            SELECT
+                *
+            FROM
+                pubsub.olist_pubsub
+            WHERE
+                publish_time > TO_TIMESTAMP('{publish_last_value}', 'YYYY-MM-DD HH24:MI:SS.MS')
+            """
+        )
+    postgres_task.execute(context={})
 
 branch_task = BranchPythonOperator(
     task_id="branch_task",
@@ -79,11 +60,6 @@ branch_task = BranchPythonOperator(
 end_task = DummyOperator(
     task_id="end_task",
     dag=dag
-)
-postgres_task = PythonOperator(
-    task_id = "get_data",
-    python_callable = message_cnt,
-    provide_context = True
 )
 
 spark_process = SparkKubernetesOperator(
@@ -99,5 +75,4 @@ spark_process = SparkKubernetesOperator(
 )
 
 get_data >> branch_task
-branch_task >> [save_to_csv, end_task]
-save_to_csv >> spark_process
+branch_task >> [spark_process, end_task]
--- spark-jobs/read_json.py
+++ spark-jobs/read_json.py
@@ -1,4 +1,4 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession,functions as F
 import json,os,datetime,shutil,time
 from pyspark.sql.functions import col,to_timestamp,unix_timestamp,floor
 from functools import reduce
@@ -47,8 +47,6 @@ if __name__ == "__main__":
     target_dir = "/opt/spark/jars/postgresql-42.7.5.jar"
     os.system("cp /opt/spark/data/postgresql-42.7.5.jar /opt/spark/jars")
     shutil.copy(source_file, target_dir)
-    time.sleep(300000)
-    # print(os.listdir("/opt/spark/jars"))
     last_value_file_path = "/opt/spark/data/publish_last_value.txt"
     spark = (
         SparkSession.builder
@@ -62,7 +60,6 @@ if __name__ == "__main__":
         .getOrCreate()
         )
     publish_last_value = get_last_value(last_value_file_path)
-    # spark = spark_sess()
     sql = f"SELECT * FROM pubsub.olist_pubsub WHERE publish_time > TO_TIMESTAMP('{publish_last_value}', 'YYYY-MM-DD HH24:MI:SS.MS') ORDER BY publish_time DESC"
 
     df = spark.read.format("jdbc") \
@@ -72,25 +69,23 @@ if __name__ == "__main__":
                 .option("user", "postgres") \
                 .option("password", "postgres") \
                 .load()
-    df.show()
-    # #last valeu 불러오기
-    # publish_last_value = get_last_value(last_value_file_path)
-    # print(publish_last_value)
-    # #sql 호출
-    # sql = f"SELECT * FROM pubsub.olist_pubsub WHERE publish_time > TO_TIMESTAMP('{publish_last_value}', 'YYYY-MM-DD HH24:MI:SS.MS') ORDER BY publish_time DESC"
-    # df = get_df(sql)
-    # #last value 갱신
-    # new_last_value = df.first()["publish_time"]
-    # write_last_value(last_value_file_path,str(new_last_value))
+    #last value 갱신
+    new_last_value = df.first()["publish_time"]
+    write_last_value(last_value_file_path,str(new_last_value))
 
-    # column_name = ["price","freight_value","payment_value"]
-    # df = df.na.drop()
-    # df = reduce(lambda df, col: df.withColumn(col, round(df[col], 2)), column_name, df)
-    # df = df.withColumn("order_delivered_customer_date", to_timestamp(col("order_delivered_customer_date"))) \
-    #     .withColumn("order_purchase_timestamp", to_timestamp(col("order_purchase_timestamp"))) \
-    #     .withColumn("time_diff_seconds", unix_timestamp(col("order_delivered_customer_date")) - unix_timestamp(col("order_purchase_timestamp"))) \
-    #     .withColumn("delevery_time",floor(col("time_diff_seconds")/3600))
-    # df = df.drop("id","order_estimated_delivery_date","order_approved_at","order_delivered_carrier_date","delivery_attempt","ordering_key","time_diff_seconds")
-    # gcs_bucket = "olist_buckets"
-    # parquet_path = f"gs://{gcs_bucket}/orders/orders.parquet"
-    # df.write.mode("append").format("parquet").save(parquet_path)
+    column_name = ["price","freight_value","payment_value"]
+    print("##################1")
+    df = df.na.drop()
+    print("##################2")
+    df = reduce(lambda df, col: df.withColumn(col, F.round(df[col], 2)), column_name, df)
+    print("##################3")
+    df = df.withColumn("order_delivered_customer_date", to_timestamp(col("order_delivered_customer_date"))) \
+        .withColumn("order_purchase_timestamp", to_timestamp(col("order_purchase_timestamp"))) \
+        .withColumn("time_diff_seconds", unix_timestamp(col("order_delivered_customer_date")) - unix_timestamp(col("order_purchase_timestamp"))) \
+        .withColumn("delevery_time",floor(col("time_diff_seconds")/3600))
+    print("##################4")
+    df = df.drop("id","order_estimated_delivery_date","order_approved_at","order_delivered_carrier_date","delivery_attempt","ordering_key","time_diff_seconds")
+    gcs_bucket = "olist_buckets"
+    parquet_path = f"gs://{gcs_bucket}/orders/orders.parquet"
+    print("##################5")
+    df.write.mode("append").format("parquet").save(parquet_path)
