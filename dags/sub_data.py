9ac6f8862d131ee66a9e22790479d769b9d56153
aky <aky@ubuntu.myguest.virtualbox.org>
2025-03-11 17:48:36 +0900

    T15 fix: collect DAG 코드 수정

--- dags/sub_data.py
+++ dags/sub_data.py
@@ -12,7 +12,7 @@ import csv
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime.now(),
+    "start_date":datetime(2025, 3, 11, 8, 49),
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
