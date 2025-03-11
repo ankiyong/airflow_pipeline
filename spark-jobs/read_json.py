c38d9eb148d9773adba6c5409c13f11353b8d064
aky <aky@ubuntu.myguest.virtualbox.org>
2025-03-11 15:57:53 +0900

    T15: fix - 데이터 파이프라인 수정

--- spark-jobs/read_json.py
+++ spark-jobs/read_json.py
@@ -4,21 +4,6 @@ from pyspark.sql.functions import col,to_timestamp,unix_timestamp,floor
 from functools import reduce
 from dotenv import load_dotenv
 
-
-def spark_sess():
-    spark = (
-        SparkSession.builder
-        .appName("pyspark-gcs-connection")
-        .master("local[*]")
-        .config("spark.jars", "/opt/spark/data/gcs-connector-hadoop3-2.2.9-shaded.jar")
-        .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
-        .config("spark.hadoop.google.cloud.auth.service.account.enable", "true")
-        .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", "/opt/spark/data/key.json")
-        .config("spark.driver.extraClassPath", "/opt/spark/data/postgresql-42.7.5.jar") \
-        .getOrCreate()
-        )
-    return spark
-
 def get_last_value(file_path):
     if not os.path.exists(file_path):
         publish_last_value = "2000-01-01 00:00:00.000"
@@ -71,21 +56,18 @@ if __name__ == "__main__":
                 .load()
     #last value 갱신
     new_last_value = df.first()["publish_time"]
-    write_last_value(last_value_file_path,str(new_last_value))
+    dt = datetime.strptime(new_last_value, "%Y-%m-%d %H:%M:%S.%f")
+    publish_last_value = dt.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    write_last_value(last_value_file_path,str(publish_last_value))
 
     column_name = ["price","freight_value","payment_value"]
-    print("##################1")
     df = df.na.drop()
-    print("##################2")
     df = reduce(lambda df, col: df.withColumn(col, F.round(df[col], 2)), column_name, df)
-    print("##################3")
     df = df.withColumn("order_delivered_customer_date", to_timestamp(col("order_delivered_customer_date"))) \
         .withColumn("order_purchase_timestamp", to_timestamp(col("order_purchase_timestamp"))) \
         .withColumn("time_diff_seconds", unix_timestamp(col("order_delivered_customer_date")) - unix_timestamp(col("order_purchase_timestamp"))) \
         .withColumn("delevery_time",floor(col("time_diff_seconds")/3600))
-    print("##################4")
     df = df.drop("id","order_estimated_delivery_date","order_approved_at","order_delivered_carrier_date","delivery_attempt","ordering_key","time_diff_seconds")
-    gcs_bucket = "olist_buckets"
+    gcs_bucket = "olist_data_buckets"
     parquet_path = f"gs://{gcs_bucket}/orders/orders.parquet"
-    print("##################5")
     df.write.mode("append").format("parquet").save(parquet_path)
